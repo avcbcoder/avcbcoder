a305cb61832dc97274f3e9597b87c6da9efaa3c2
Ankit Verma <ankitvermamat@gmail.com>
2019-02-01 16:36:02 +0530

    modified js for animation

--- js/accordion-js.js
+++ js/accordion-js.js
@@ -1,27 +1,49 @@
+var educationTitle = ["BACHELOR DEGREE OF COMPUTER SCIENCE", "HIGH SCHOOL SECONDARY EDUCATION", ""];
 window.onload = function () {
     var accordionTitle = document.getElementsByClassName('accordion-title');
-    var accordionContent = document.getElementsByClassName('accordion-content');
+    var accordionContent = [];
+    // document.getElementsByClassName('accordion-content');
+    // Set title
     for (let i = 0; i < accordionTitle.length; i++) {
+        accordionTitle[i].innerText = educationTitle[i]
+        accordionContent.push(accordionTitle[i].nextElementSibling)
+    }
+    // Set content
+
+    var currentAccordion = 0;
+    accordionTitle[0].classList.toggle("active")
+
+    animateAccordion(currentAccordion);
+
+    for (let i = 0; i < accordionTitle.length; i++) { // add click listener for each panel heading
         accordionTitle[i].addEventListener("click", function () {
-            console.log('click')
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.maxHeight) { // hide content
-                content.style.maxHeight = null;
-                content.style.border = "0px solid #E6E6E6";
-                content.style.marginBottom = "0px";
-            }
-            else {
-                for (let j = 0; j < 3; j++) {// hide all content
-                    accordionContent[j].style.maxHeight = null;
-                    accordionContent[j].style.border = "0px solid #E6E6E6";
-                    accordionContent[j].style.marginBottom = "0px";
-                }
-                // show the one which is clicked
-                content.style.maxHeight = content.scrollHeight + "px";
-                content.style.border = "1px solid #E6E6E6";
-                content.style.marginBottom = "5px";
-            }
+            var newAccordion = -1;
+            for (let j = 0; j < 3; j++)  // set newAccordion to the one which is clicked
+                if (accordionTitle[j] == this)
+                    newAccordion = j;
+            // toggle currently clicked
+            accordionTitle[newAccordion].classList.toggle("active")
+            // toggle previous if its not the same 
+            if (newAccordion != currentAccordion)
+                accordionTitle[currentAccordion].classList.toggle("active")
+            currentAccordion = newAccordion
+            animateAccordion(currentAccordion)
         });
+    }
+
+    function animateAccordion(curr) {
+        var show = 1;
+        if (accordionContent[curr].style.maxHeight) // show if height is 0 otherwise just hide
+            show = 0
+        for (let j = 0; j < 3; j++) {// hide all content
+            accordionContent[j].style.maxHeight = null;
+            accordionContent[j].style.border = "0px solid #E6E6E6";
+            accordionContent[j].style.marginBottom = "0px";
+        }
+        if (show == 1) {
+            accordionContent[curr].style.maxHeight = accordionContent[curr].scrollHeight + "px";
+            accordionContent[curr].style.border = "1px solid #E6E6E6";
+            accordionContent[curr].style.marginBottom = "5px";
+        }
     }
 }
